90446f1404847160ae2f2f7e4ed71969d053bec6
Sujal-Shreshya <ishub515@gmail.com>
2026-03-20 13:11:05 +0530

    Removed old files

--- prdemo/Mathoperation.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class Mathoperation {
-    public static void main(String[] args) {
-    Mathoperation m= new Mathoperation();
-    System.out.println("Sum of two int:" + m.add(5, 10));
-    System.out.println("Sum of three int" + m.add(5, 10, 15));
-    System.out.println("Sum of two double:" + m.add(5.5, 10.5));
-    }
-}
